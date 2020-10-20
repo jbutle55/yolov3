5315d0bb77e846402e18aa0b741a0099789f9728
jbutle55 <jbutle55@uwo.ca>
2020-10-20 07:51:38 -0700

    Fix folder paths

--- alter_data_txt_file.py
+++ alter_data_txt_file.py
@@ -7,5 +7,5 @@ new_file = 'out.txt'
 with open(txt_file) as fin:
     with open(new_file, 'w') as fout:
         for line in fin:
-            line = line.replace('./images/', '/data2/DATA_justin/COCO2017/')
+            line = line.replace('.', '/data2/DATA_justin/COCO2017/')
             fout.write(line)
