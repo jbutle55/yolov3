e724df722726e98f8a83072e49c0491c2b2b6091
jbutle55 <jbutle55@uwo.ca>
2020-10-30 11:05:11 -0600

    Alter to create tt file for Stanford Dataset

--- alter_data_txt_file.py
+++ alter_data_txt_file.py
@@ -2,11 +2,15 @@ import os
 import sys
 
 txt_file = sys.argv[1]
-new_file = 'out.txt'
+#new_file = 'out.txt'
+new_file = sys.argv[2]
 
 with open(txt_file) as fin:
     with open(new_file, 'w') as fout:
         for line in fin:
-            line = line.lstrip('.')
-            line = '/Users/justinbutler/Desktop/school/Calgary/ML_Work/Datasets/COCO2017' + line
+            #line = line.lstrip('.')
+            #line = '/Users/justinbutler/Desktop/school/Calgary/ML_Work/Datasets/COCO2017' + line
+
+            line = '/data2/DATA_justin/stanford_dataset/sdd/JPEGImages/' + line + '.jpg'
+
             fout.write(line)
